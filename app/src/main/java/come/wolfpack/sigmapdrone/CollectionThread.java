77b84e1702ce61ed61b8846c8d4b2367ff48131e
Max McKinley <MaxWMcKinley@gmail.com>
2017-10-02 02:59:47 -0500

    Added power management and improved thread handling

---
+++ app/src/main/java/come/wolfpack/sigmapdrone/CollectionThread.java
@@ -0,0 +1,62 @@
+package come.wolfpack.sigmapdrone;
+
+import android.content.Context;
+import android.os.PowerManager;
+import android.util.Log;
+
+/**
+ * Created by max on 10/1/17.
+ */
+
+public class CollectionThread implements Runnable {
+    private static final String TAG = "CollectionThread";
+
+    private Thread thread;
+    private static DbHelper dbHelper;
+    private static WifiHelper wifiHelper;
+
+    private static PowerManager pm;
+    private static PowerManager.WakeLock wl;
+
+    public CollectionThread(Context context) {
+        this.dbHelper = new DbHelper(context);
+        this.wifiHelper = new WifiHelper(context);
+
+        this.pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+        this.wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+    }
+
+    public void start() {
+        wl.acquire();
+
+        if( thread == null ) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    public void stop() {
+        wl.release();
+
+        if( thread != null ) {
+            thread.interrupt();
+        }
+    }
+
+    public void run() {
+        try {
+            Log.i(TAG, "Thread starting.");
+            while( !thread.interrupted() ) {
+                collectWifi();
+            }
+            Log.i(TAG, "Thread stopping.");
+        } finally {
+            thread = null;
+        }
+    }
+
+    public void collectWifi () {
+        int rssi = wifiHelper.getRSSI();
+        dbHelper.insertSignal(rssi);
+    }
+}
--- app/src/main/java/come/wolfpack/sigmapdrone/MainActivity.java
+++ app/src/main/java/come/wolfpack/sigmapdrone/MainActivity.java
@@ -8,50 +8,88 @@ import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Context context;
+    private CollectionThread thread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = getApplicationContext();
+        thread = new CollectionThread(context);
+    }
+
+    public static Context getContext() {
+        return context;
     }
 
     public void collectWifi (View view) {
 
-        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "My Tag");
-        wl.acquire();
+        Button button = (Button)findViewById(R.id.button);
+        button.setText("Stop");
 
-        Context context = getApplicationContext();
-
-        final TextView ssidText = (TextView) findViewById(R.id.textView);
-        final TextView rssiText = (TextView) findViewById(R.id.textView2);
-
-        final Handler handler = new Handler();
-        final int delay = 100;
-
-        final DbHelper dbHelper = new DbHelper(context);
-        final WifiHelper wifiHelper = new WifiHelper(context);
-
-        String ssid = wifiHelper.getSSID();
-        ssidText.setText("Connected: " + ssid);
-
-        // TODO: Use a thread pool manager to execute runnables
-        handler.postDelayed(new Runnable(){
-            public void run(){
-                int rssi = wifiHelper.getRSSI();
-                rssiText.setText("Wifi Strength: " + String.valueOf(rssi) + " dBm");
-
-                dbHelper.insertSignal(rssi);
-
-                handler.postDelayed(this, delay);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopCollection(view);
             }
-        }, delay);
+        });
+
+        thread.start();
     }
+
+    public void stopCollection(View view) {
+        thread.stop();
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setText("Start");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collectWifi(view);
+            }
+        });
+    }
+
+//        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+//        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "My Tag");
+//        wl.acquire();
+//
+//        Context context = getApplicationContext();
+//
+//        final TextView ssidText = (TextView) findViewById(R.id.textView);
+//        final TextView rssiText = (TextView) findViewById(R.id.textView2);
+//
+//        final Handler handler = new Handler();
+//        final int delay = 100;
+//
+//        final DbHelper dbHelper = new DbHelper(context);
+//        final WifiHelper wifiHelper = new WifiHelper(context);
+//
+//        String ssid = wifiHelper.getSSID();
+//        ssidText.setText("Connected: " + ssid);
+//
+//        // TODO: Use a thread pool manager to execute runnables
+//        handler.postDelayed(new Runnable(){
+//            public void run(){
+//                int rssi = wifiHelper.getRSSI();
+//                rssiText.setText("Wifi Strength: " + String.valueOf(rssi) + " dBm");
+//
+//                dbHelper.insertSignal(rssi);
+//
+//                handler.postDelayed(this, delay);
+//            }
+//        }, delay);
+//    }
 
     public void viewDatabase(View view) {
         Intent intent = new Intent(this, ViewDatabaseActivity.class);
